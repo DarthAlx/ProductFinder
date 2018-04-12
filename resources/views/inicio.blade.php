da2166628607c34b9171affc886b883512b1b3e0
Alexis Morales <alx.morales@hadoukendev.com>
2018-04-11 22:20:00 -0500

    doble linea

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -133,7 +133,7 @@ $items=Cart::content();
                             </div>
                           </div>
                           <div class="name">
-                            <b>{{str_limit($producto['nombre'], $limit = 17, $end = '...')}}</b>
+                            <b>{{str_limit($producto['nombre'], $limit = 35, $end = '...')}}</b>
                           </div>
                           <div class="pricefrom">
                             <div class="price">$  {!!number_format($producto['precio']/100, 2, '.', ',')!!}</div>
@@ -252,7 +252,7 @@ $items=Cart::content();
                             </div>
                           </div>
                           <div class="name">
-                            <b>{{str_limit($producto['nombre'], $limit = 30, $end = '...')}}</b>
+                            <b>{{str_limit($producto['nombre'], $limit = 35, $end = '...')}}</b>
                           </div>
                           <div class="pricefrom">
                             <div class="price">$  {!!number_format($producto['precio']/100, 2, '.', ',')!!}</div>
