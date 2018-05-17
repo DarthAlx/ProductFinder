063ebba90a3634c8efa164ad097296b74f1d9585
Alexis Morales <alx.morales@hadoukendev.com>
2018-05-17 16:56:28 -0500

    Jab price

--- resources/views/acercade.blade.php
+++ resources/views/acercade.blade.php
@@ -22,9 +22,9 @@
           <div class="row">
             <div class="col-md-12 text-justify">
               <p>Jab Price es uno de los comparadores de precios más destacados de México con millones de productos a comparar. El gran número de tiendas y ofertas que consideramos aumenta las posibilidades de que encuentres el precio más barato del producto que buscas. </p>
-              <p>En Product Finder contamos con cientos de análisis de productos y opiniones de usuarios. Con ello, podrás encontrar no solo el mejor precio, sino también útiles consejos que te ayudarán a decidirte por el mejor producto. </p>
+              <p>En Jab Price contamos con cientos de análisis de productos y opiniones de usuarios. Con ello, podrás encontrar no solo el mejor precio, sino también útiles consejos que te ayudarán a decidirte por el mejor producto. </p>
               <p>Contamos con una amplia gama de trabajadores que se encargan de actualizar la información acerca de los productos y notificar a los usuarios cuando sus productos favoritos alcanzan un menor precio.</p>
-              <p>En Product Finder no se compra la primera posición. La posición se determina dependiendo la relevancia a la búsqueda y su precio más bajo. Todas las tiendas tienen la opción de ocupar la primera posición de la lista ofreciendo los precios más bajos respecto del mismo producto. Los precios y ofertas de product finder se actualizan cada segundo para que la disponibilidad y el precio de las ofertas sean correctos.</p>
+              <p>En Jab Price no se compra la primera posición. La posición se determina dependiendo la relevancia a la búsqueda y su precio más bajo. Todas las tiendas tienen la opción de ocupar la primera posición de la lista ofreciendo los precios más bajos respecto del mismo producto. Los precios y ofertas de Jab Price se actualizan cada segundo para que la disponibilidad y el precio de las ofertas sean correctos.</p>
             </div>
             
           </div>
